c440a8869aca35f7b41ccf81f08357f07b10de08
swadhinjit-das <swadhinjit@credencesoft.in>
2026-08-11 21:48:07 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
       <WhyChooseUs />
       <Testimonials />
       <Gallery />
-      <Dining />
+      {/* <Dining /> */}
       <NearbyAttractions />
       <FAQ />
       <CTA
